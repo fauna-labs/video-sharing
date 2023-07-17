60215a7d2d436da13717b26b7d963e5ee14cee7b
shadid12 <shadidhaque2012@gmail.com>
2023-07-17 15:15:03 -0400

    recorder page phase 1

---
+++ src/app/record/page.js
@@ -0,0 +1,17 @@
+'use client'
+import Link from 'next/link';
+import styles from './recorder.module.css';
+
+export default function Record() {
+
+    const handleRecordedVid = async (blob) => {
+        // TODO: Store video metadata in Fauna
+        // TODO: Store video blob in Cloundflare R2 Storage
+    }
+    return (
+        <div className={styles.mainWrap}>
+            <Link href='/'><span className={styles.home}>🏠 Home</span></Link>
+            <h1>Record Video</h1>
+        </div>
+    )
+}
