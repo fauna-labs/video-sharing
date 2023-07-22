108ad105d2288f9a957fa51b915f93d3e32d47f5
shadid12 <shadidhaque2012@gmail.com>
2023-07-22 13:59:18 -0400

    make video share

--- src/app/video/[id]/page.js
+++ src/app/video/[id]/page.js
@@ -24,6 +24,8 @@ export default function VideoPage() {
     });
     const [canSeeContent, setCanSeeContent] = useState(false);
     const [video, setVideo] = useState(null);
+    const [isOwner, setIsOwner] = useState(false);
+    const [email, setEmail] = useState("");
 
     if(!userInfo) {
         return (
@@ -56,15 +58,51 @@ export default function VideoPage() {
             if (response.data.is_owner || response.data.permission.data.length > 0) {
                 setCanSeeContent(true)
                 setVideo(response.data.video)
+                setIsOwner(response.data.is_owner)
             }
         })
     };
+
+    const handleEmailChange = (e) => {
+        setEmail(e.target.value);
+    }
+
+    const handleShareFormSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const response = await client.query(fql`
+                let video = Video.byId(${id})
+                let user = User.where(.email == ${email}).first()
+                Permission.create({
+                    user: user,
+                    video: video
+                })
+            `)
+            alert('Video shared successfully')
+            setEmail("")
+        } catch (error) {
+            alert('There was an error sharing your video')
+        }
+    }
 
     if (canSeeContent) {
         return (
             <div className={styles.main}>
                 <h1>{video.title}</h1>
                 <video controls width="600px" height="300px" src={`https://backend.shadidhaque2014.workers.dev/${video.id}`} />
+                <hr />
+                {
+                    isOwner && (
+                        <div>
+                            <form onSubmit={handleShareFormSubmit}>
+                                <label htmlFor="email">Share with:</label> 
+                                <br />
+                                <input type="email" value={email} onChange={handleEmailChange} />
+                                <button type="submit">Share</button>
+                            </form>
+                        </div>
+                    )
+                }
             </div>
         )
     }
