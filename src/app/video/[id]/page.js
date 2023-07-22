44b798b6b6c05ea8fd23dd33fa48243b53aa9ee6
shadid12 <shadidhaque2012@gmail.com>
2023-07-22 13:49:12 -0400

    video visibility

---
+++ src/app/video/[id]/page.js
@@ -0,0 +1,72 @@
+'use client'
+import { useEffect, useState } from "react";
+import { useRouter, useParams } from 'next/navigation';
+import { Client, fql } from "fauna";
+import styles from  './page.module.css';
+
+
+/** This component does the following:
+ * 1. Get video
+ * 2. Check if video owner
+ *  - if owner, show the video
+ *  - if not owner, check if video is shared with user
+ * 3. Check if video is shared with user
+ *  - if shared, show the video
+ *  - if not shared, show error message "You do not have permission to view this video"
+ */
+
+export default function VideoPage() {
+    const router = useRouter();
+    const { id } = useParams();
+    const userInfo = JSON.parse(localStorage.getItem("video-sharing-app"));
+    const client = new Client({
+        secret: userInfo ? userInfo.key : process.env.NEXT_PUBLIC_FAUNA_KEY
+    });
+    const [canSeeContent, setCanSeeContent] = useState(false);
+    const [video, setVideo] = useState(null);
+
+    if(!userInfo) {
+        return (
+            <div>
+                <p>Please Log In</p>
+                <button onClick={() => router.push("/login")}>Log In</button>
+            </div>
+        )
+    }
+
+    useEffect(() => {
+        videoVisibility();
+    }, [id]);
+
+    const videoVisibility = () => {
+        client.query(fql`
+            let video = Video.byId(${id})
+            let user = User.byId(${userInfo.id})
+            let permission = Permission.where(.user == user && .video == video)
+            
+            let result = {
+                video: video,
+                permission: permission,
+                is_owner: video.author == user
+            }
+
+            result
+        `).then((response) => {
+            console.log('response', response.data)
+            if (response.data.is_owner || response.data.permission.data.length > 0) {
+                setCanSeeContent(true)
+                setVideo(response.data.video)
+            }
+        })
+    };
+
+    if (canSeeContent) {
+        return (
+            <div className={styles.main}>
+                <h1>{video.title}</h1>
+                <video controls width="600px" height="300px" src={`https://backend.shadidhaque2014.workers.dev/${video.id}`} />
+            </div>
+        )
+    }
+    
+}
