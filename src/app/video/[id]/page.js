94b921f492cfddfeace3efd6300bd7c66ae7c74e
shadid12 <shadidhaque2012@gmail.com>
2023-07-24 11:38:39 -0400

    update runtime

--- src/app/video/[id]/page.js
+++ src/app/video/[id]/page.js
@@ -1,4 +1,5 @@
 'use client'
+export const runtime = 'edge';
 import { useEffect, useState } from "react";
 import { useRouter, useParams } from 'next/navigation';
 import { Client, fql } from "fauna";
